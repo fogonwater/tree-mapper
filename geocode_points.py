e1ca0950fcd42f7cebe09c8df667a18950b12dd1
Chris McDowall <chris.mcdowall@gmail.com>
2016-07-17 14:12:02 +1200

    Made locator class. Switched outputs to geojson.

--- geocode_points.py
+++ geocode_points.py
@@ -5,6 +5,51 @@ import geocoder
 
 SRC_ADDRESSES = 'data/addresses.csv'
 DST_GEOCODED = 'data/geocode.csv'
+COUNT_FIELDS = ["Plums ","Frank's Early ","Wilson's Early ","Black Doris "]
+
+
+class Location:
+    def __init__(self, loc_string, fields=[], row_dic={}, verbose=True):
+        self.loc_string = contextualise(loc_string)
+        self.properties = {'name':loc_string}
+        self.read_properties(fields, row_dic)
+        self.located = False
+        self.verbose = verbose
+        self.geocode()
+
+    def read_properties(self, fields, row_dic):
+        for field in fields:
+            key = clean_key(field)
+            if row_dic[field]:
+                self.properties[key] = int(row_dic[field])
+            else:
+                self.properties[key] = 0
+
+    def geocode(self):
+        time.sleep(1)
+        g = geocoder.google(self.loc_string)
+        coors = g.latlng
+        try:
+            self.lat, self.lng = float(coors[0]), float(coors[1])
+            self.located = True
+        except IndexError:
+            self.lat, self.lng = None, None
+            self.located = False
+        if self.verbose:
+            print('{} -> {}, {}'.format(self.loc_string, self.lat, self.lng))
+
+    def geojson(self):
+        if not self.located:
+            return None
+        return {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [self.lng, self.lat],
+            },
+            'properties':self.properties
+        }
+
 
 ### UTILITIES
 
@@ -13,6 +58,64 @@ def read_csv(f_path):
         reader = csv.DictReader(f)
         rows = [ row for row in reader]
     return rows
+
+def write_json(data, f_path):
+    with open(f_path, 'w') as outfile:
+        json.dump(
+            data,
+            outfile,
+            indent=2,
+            sort_keys=True
+        )
+    print('{} written.'.format(f_path))
+
+def clean_key(s):
+    key = s.strip()
+    key = key.replace(' ', '_')
+    key = key.replace("'", '')
+    return key.lower()
+
+def contextualise(loc, context='Waiheke Island, NZ'):
+    return '{}, {}'.format(loc, context.strip())
+
+
+
+
+### CONTROL THING
+
+def main():
+    rows = read_csv(SRC_ADDRESSES)
+    features = {'type': 'FeatureCollection', 'features':[]}
+    for row in rows:
+        raw_address = row['Address']
+        if not raw_address:
+            continue
+        loc = Location(raw_address, fields=COUNT_FIELDS, row_dic=row)
+        if loc.located:
+            features['features'].append(loc.geojson())
+        else:
+            print('* UNABLE TO GEOCODE: {}'.format(raw_address))
+
+    write_json(features, 'data/geocode.json')
+
+    '''
+    write_report(items, DST_GEOCODED)
+    build_report(items, 'templates/index-template.html')
+    print('')
+    print('* Errors:')
+    pp(errors)
+    '''
+
+if __name__ == '__main__':
+    main()
+
+'''
+
+def safe_int(s):
+    try:
+        return int(s)
+    except ValueError:
+        return 0
 
 def write_report(items, f_path):
     headers = items[0].keys()
@@ -28,31 +131,17 @@ def write_report(items, f_path):
         writer.writerows(rows)
     print('Wrote {}.'.format(f_path))
 
-### GEOCODING AND DATA CLEANING
-
-def geocode(location):
-    g = geocoder.google(location)
-    return g.latlng
-
-def contextualise(loc, context='Waiheke Island, NZ'):
-    return '{},{}'.format(loc, context)
-
-def clean_lat_lng(coors):
-    try:
-        return [float(coors[0]), float(coors[1])]
-    except IndexError:
-        return [None, None]
 
 ### GENERATE REPORT
 
 def get_jinja_template(template_file):
-    ''' This loads a jinja template file. '''
+    # This loads a jinja template file. #
     loader = jinja2.FileSystemLoader(searchpath=".")
     env = jinja2.Environment(loader=loader)
     return env.get_template(template_file)
 
 def build_report(items, index_template):
-    ''' Takes a list of items & builds an HTML page from the template'''
+    # Takes a list of items & builds an HTML page from the template#
     template = get_jinja_template(index_template)
     doc = template.render({
         'items':items
@@ -75,34 +164,4 @@ def generate_map(coors, width=150, height=150, zoom=12):
         "&markers=size:mid%7Ccolor:0xff0000%7Clabel:%7C",
         loc,
     ])
-
-### CONTROL THING
-
-def main():
-    rows = read_csv(SRC_ADDRESSES)
-    items = []
-    errors = []
-    for i, row in enumerate(rows):
-        raw_address = row['Address']
-        if not raw_address:
-            continue
-        #if i >= 3: break
-        address = contextualise(raw_address)
-        coors = clean_lat_lng( geocode(address) )
-        print('{} : {}'.format(address, coors))
-        time.sleep(1)
-        items.append({
-            'address':address,
-            'lat':coors[0],
-            'lng':coors[1],
-            'map':generate_map(coors),
-        })
-
-    write_report(items, DST_GEOCODED)
-    build_report(items, 'templates/index-template.html')
-    print('')
-    print('* Errors:')
-    pp(errors)
-
-if __name__ == '__main__':
-    main()
+'''
