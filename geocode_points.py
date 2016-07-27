1fc96fba98116ab2f2b83d68144ed8c616fe136b
Chris McDowall <chris.mcdowall@gmail.com>
2016-07-27 18:49:23 +1200

    Cleaned up geocoder

--- geocode_points.py
+++ geocode_points.py
@@ -3,8 +3,8 @@ import jinja2
 from pprint import pprint as pp
 import geocoder
 
-SRC_ADDRESSES = 'data/addresses.csv'
-DST_GEOCODED = 'data/geocode.csv'
+SRC_ADDRESSES = 'data/addresses.csv' # this is the input csv
+DST_GEOCODED = 'data/geocode.json'   # this is the output
 COUNT_FIELDS = ["Plums ","Frank's Early ","Wilson's Early ","Black Doris "]
 
 
@@ -52,7 +52,6 @@ class Location:
 
 
 ### UTILITIES
-
 def read_csv(f_path):
     with open(f_path) as f:
         reader = csv.DictReader(f)
@@ -79,89 +78,22 @@ def contextualise(loc, context='Waiheke Island, NZ'):
     return '{}, {}'.format(loc, context.strip())
 
 
-
-
 ### CONTROL THING
 
 def main():
     rows = read_csv(SRC_ADDRESSES)
-    features = {'type': 'FeatureCollection', 'features':[]}
+    geojson = {'type': 'FeatureCollection', 'features':[]}
     for row in rows:
         raw_address = row['Address']
         if not raw_address:
             continue
         loc = Location(raw_address, fields=COUNT_FIELDS, row_dic=row)
         if loc.located:
-            features['features'].append(loc.geojson())
+            geojson['features'].append(loc.geojson())
         else:
             print('* UNABLE TO GEOCODE: {}'.format(raw_address))
 
-    write_json(features, 'data/geocode.json')
-
-    '''
-    write_report(items, DST_GEOCODED)
-    build_report(items, 'templates/index-template.html')
-    print('')
-    print('* Errors:')
-    pp(errors)
-    '''
+    write_json(geojson, DST_GEOCODED)
 
 if __name__ == '__main__':
     main()
-
-'''
-
-def safe_int(s):
-    try:
-        return int(s)
-    except ValueError:
-        return 0
-
-def write_report(items, f_path):
-    headers = items[0].keys()
-    headers.sort()
-    rows = [headers]
-    for item in items:
-        row = []
-        for header in headers:
-            row.append(item[header])
-        rows.append(row)
-    with open(f_path, 'wb') as f:
-        writer = csv.writer(f)
-        writer.writerows(rows)
-    print('Wrote {}.'.format(f_path))
-
-
-### GENERATE REPORT
-
-def get_jinja_template(template_file):
-    # This loads a jinja template file. #
-    loader = jinja2.FileSystemLoader(searchpath=".")
-    env = jinja2.Environment(loader=loader)
-    return env.get_template(template_file)
-
-def build_report(items, index_template):
-    # Takes a list of items & builds an HTML page from the template#
-    template = get_jinja_template(index_template)
-    doc = template.render({
-        'items':items
-    })
-    with open('index.html', 'w') as f:
-        f.write(doc)
-
-def generate_map(coors, width=150, height=150, zoom=12):
-    loc = "{},{}".format(coors[0], coors[1])
-    return ''.join([
-        "http://maps.googleapis.com/maps/api/staticmap?center=",
-        loc,
-        "&zoom=",
-        str(zoom),
-        "&scale=false&size=",
-        str(width),
-        "x",
-        str(height),
-        "&maptype=roadmap&format=png&visual_refresh=true"
-        "&markers=size:mid%7Ccolor:0xff0000%7Clabel:%7C",
-        loc,
-    ])
-'''
